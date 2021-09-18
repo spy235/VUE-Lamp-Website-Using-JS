d9976580384969a7e01ab48f1f08ca9fa97d7d57
spy-byte <spyangel235@gmail.com>
2021-09-18 23:29:35 +0530

    vue-lighting-using js

---
+++ script.js
@@ -0,0 +1,8 @@
+// basic  Javascript code
+let btn = document.getElementById("btn");
+let lyt= document.getElementById("lyt2");
+let rot=document.getElementById("lyt1");
+function tooglebtn(){
+    btn.classList.toggle("active");
+    lyt.classList.toggle("on");
+} 
